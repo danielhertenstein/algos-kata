75d04bfa0b739859629989da60f6474f6bc2eed3
Daniel Hertenstein <daniel.hertenstein@proton.me>
2023-05-10 16:29:43 -0400

    TwoCrystalBalls no multiplication impl

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,19 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
     // Jump sqrt(breaks.length) until breaks
     const stride = Math.floor(Math.sqrt(breaks.length));
-    const number_of_strides = Math.floor(breaks.length / stride);
-    for (let i = 1; i <= number_of_strides; ++i) {
-        if (breaks[stride * i]) {
-            // Walk from previous jump forward
-            for (let j = stride * (i - 1) + 1; j <= stride * i; ++j) {
-                if (breaks[j]) {
-                    return j;
-                }
-            }
+    let i = stride;
+    for (; i <= breaks.length; i += stride) {
+        if (breaks[i]) {
+            break;
         }
     }
-    // Has to break in remaining floors
-    for (let i = stride * number_of_strides + 1; i < breaks.length; ++i) {
+
+    i -= stride;
+
+    for (let j = 0; j <= stride && i < breaks.length; ++j, ++i) {
         if (breaks[i]) {
             return i;
         }
