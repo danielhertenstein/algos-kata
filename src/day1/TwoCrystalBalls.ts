3cb6601bf63b91bc2d48b161b93e19d93d2894c3
Daniel Hertenstein <daniel.hertenstein@proton.me>
2023-05-10 16:21:05 -0400

    TwoCrystallBalls ugly impl

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // Jump sqrt(breaks.length) until breaks
+    const stride = Math.floor(Math.sqrt(breaks.length));
+    const number_of_strides = Math.floor(breaks.length / stride);
+    for (let i = 1; i <= number_of_strides; ++i) {
+        if (breaks[stride * i]) {
+            // Walk from previous jump forward
+            for (let j = stride * (i - 1) + 1; j <= stride * i; ++j) {
+                if (breaks[j]) {
+                    return j;
+                }
+            }
+        }
+    }
+    // Has to break in remaining floors
+    for (let i = stride * number_of_strides + 1; i < breaks.length; ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
 
+    return -1;
 }
