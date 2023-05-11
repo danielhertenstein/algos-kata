d86a6afbfc230f272ee4f03bd2fe10db4b3a57f5
Daniel Hertenstein <daniel.hertenstein@proton.me>
2023-05-10 22:10:27 -0400

    Stack impl

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,36 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>;
+        node.next = this.head;
+        this.head = node;
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const node = this.head;
+        this.head = node.next;
+        this.length--;
+        return node.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
